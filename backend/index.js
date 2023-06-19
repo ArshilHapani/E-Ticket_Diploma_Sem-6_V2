35807dcae36814b6de4bc8d876da2aa7d8addcd7
ArshilHapani <arshilhapani998@gmail.com>
2023-06-19 14:41:37 +0530

    CORS error fixing

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,11 @@ con.connect((error) => {
     console.log("connected");
   }
 });
+app.all("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 app.use(cors());
 app.get("/", (req, res) => {
